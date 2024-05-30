5d4bc28513a6be33bf5d92fdd96228ca3908ba97
mosahle7 <sahlethappi@gmail.com>
2024-05-30 13:52:16 +0530

    SpaceX Initial Setup!

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,4 @@
-
+const axios = require('axios');
 // const launches = new Map();
 const launchesDB = require('./launches.mongo');
 const planets = require('./planets.mongo')
@@ -6,17 +6,67 @@ const planets = require('./planets.mongo')
 const  DEFAULT_FLGHT_NUMBER = 100;
 
 const launch = {
-    flightNumber: DEFAULT_FLGHT_NUMBER,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customes: ['ZTM','NASA'],
-    upcoming: true,
-    success: true,
+    flightNumber: DEFAULT_FLGHT_NUMBER,  //flight_number
+    mission: 'Kepler Exploration X', //name
+    rocket: 'Explorer IS1', //rocket.name
+    launchDate: new Date('December 27, 2030'), //date_local
+    target: 'Kepler-442 b', //not applicable
+    customers: ['ZTM','NASA'], //payload.customers for each payload
+    upcoming: true, //upcoming
+    success: true, //success
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
+
+async function loadLaunchesData() {
+    console.log('Downloading launch data...');
+
+    const response = await axios.post(SPACEX_API_URL, {
+        query: {},
+        options: {
+        populate: [
+            {
+                path: 'rocket',
+                select: {
+                    name: 1
+                }
+            },
+            {
+                path: 'payloads',
+                select: {
+                    customers: 1
+                }
+            }
+        ]
+    }
+        
+    })
+
+    const launchDocs = response.data.docs;
+
+    for (const launchDoc of launchDocs) {
+        const payloads = launchDoc['payloads'];
+        const customers = payloads.flatMap((payload) => {
+            return payload['customers'];
+        })
+
+        const launch =  {
+            flightNumber: launchDoc['flight_number'],  //flight_number
+            mission: launchDoc['name'], //name
+            rocket: launchDoc['rocket']['name'], //rocket.name
+            launchDate: launchDoc['date_local'], //date_local
+            target: 'Kepler-442 b', //not applicable
+            customers, //payload.customers for each payload
+            upcoming: launchDoc['upcoming'], //upcoming
+            success: launchDoc['success'], //success
+        }
+
+        console.log(`${launch.flightNumber} ${launch.mission}`)
+    }
+
+}
 
 // console.log(launch.flightNumber);
 
@@ -105,6 +155,7 @@ async function abortLaunchById(launchId) {
     return aborted.modifiedCount === 1;
 }
 module.exports = {
+    loadLaunchesData,
     getAllLaunches,
     scheduleNewLaunch,
     existLaunch,
